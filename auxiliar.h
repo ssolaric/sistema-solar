7dbebe8789f6f5c770ce1e093789ed8652c097d5
Sebastián Solari <35378033+ssolaric@users.noreply.github.com>
2017-12-21 22:23:46 -0500

    Nuevo archivo para volver a intentar el problema

---
+++ auxiliar.h
@@ -0,0 +1,37 @@
+#pragma once
+
+struct Vertex {
+    float x, y, z;
+};
+
+struct Triangle {
+    unsigned int indices[3];
+};
+
+struct Mesh {
+    //Información de estructura
+    int numVertices;
+    int numTriangles;
+    std::vector<Vertex> vertices;
+    std::vector<Triangle> triangles;
+
+    //Información para transformación inicial
+    glm::vec3 centro;
+    float scale;
+
+    //Matriz de transformación
+    glm::mat4 model_transform;
+
+    //Buffers para graficado
+    std::vector<GLfloat> object_vertices;
+    std::vector<GLfloat> object_color;
+    std::vector<GLushort> object_indexes;
+
+    //Ids para buffers
+    GLuint vbo_object;
+    GLuint vbo_color;
+    GLuint ibo_object;
+
+    Mesh(int nverts, int ntriang) : numVertices(nverts), numTriangles(ntriang), vertices(numVertices), triangles(numTriangles) {
+    }
+};
---
+++ sistema_solar.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <algorithm>
+#include <cstdio>
+#include <vector>
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include "shader_utils.h"
+#include "auxilar.h"
+
+int screen_width = 800, screen_height = 800;
+
+bool init_resources();
+void onDisplay();
+void onReshape(int width, int height);
+void onIdle();
+void free_resources();
+
+int main(int argc, char* argv[]) {
+    glutInit(&argc, argv);
+    glutInitContextVersion(2, 0);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_ALPHA | GLUT_DEPTH | GLUT_DOUBLE);
+    glutInitWindowSize(screen_width, screen_height);
+    glutCreateWindow("OpenGL");
+
+    GLenum glew_status = glewInit();
+    if (glew_status != GLEW_OK) {
+        std::cout << "Error inicializando GLEW\n";
+        exit(EXIT_FAILURE);
+    }
+
+    if (!GLEW_VERSION_2_0) {
+        std::cout << "Tu tarjeta gráfica no soporta OpenGL 2.0\n";
+        exit(EXIT_FAILURE);
+    }
+
+    if (init_resources()) {
+        glutDisplayFunc(onDisplay);
+        glutReshapeFunc(onReshape);
+        glEnable(GL_BLEND);
+        glEnable(GL_DEPTH_TEST);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        glutIdleFunc(onIdle);
+        glutMainLoop();
+    }
+
+    free_resources();
+    exit(EXIT_SUCCESS);
+}
